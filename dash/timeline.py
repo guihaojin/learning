179bb3f34ce284a6761f844d1a7eabcbf962ed43
Haojin Gui <haojin@ascend.io>
2022-08-13 10:55:10 -0700

    tweak timeline

--- dash/timeline.py
+++ dash/timeline.py
@@ -5,13 +5,13 @@ import pandas as pd
 app = Dash(__name__)
 
 df = pd.DataFrame([
-    dict(Task="Job A", Start='2009-01-01', Finish='2009-02-28'),
-    dict(Task="Job A", Start='2008-01-01', Finish='2010-02-28'),
-    dict(Task="Job B", Start='2009-03-05', Finish='2009-04-15'),
-    dict(Task="Job C", Start='2009-02-20', Finish='2009-05-30')
+    dict(Task="Job A", Start='2009-01-01', Finish='2009-02-28', n=100),
+    dict(Task="Job A", Start='2008-01-01', Finish='2010-02-28', n=10),
+    dict(Task="Job B", Start='2009-03-05', Finish='2009-04-15', n=50),
+    dict(Task="Job C", Start='2009-02-20', Finish='2009-05-30', n=20),
 ])
 
-fig = px.timeline(df, x_start="Start", x_end="Finish", y="Task")
+fig = px.timeline(df, x_start="Start", x_end="Finish", y="Task", color="n",)
 fig.update_yaxes(autorange="reversed") # otherwise tasks are listed from the bottom up
 fig.show()
 
