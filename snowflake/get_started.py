c5339732be8055210567db706d6358e53b0cee2f
Haojin Gui <haojin@ascend.io>
2022-09-07 15:29:11 -0700

    show stuff

--- snowflake/get_started.py
+++ snowflake/get_started.py
@@ -15,6 +15,21 @@ try:
     cs.execute("SELECT current_version()")
     one_row = cs.fetchone()
     print(one_row[0])
+
+    cs.execute("show warehouses")
+    rows = cs.fetchall()
+    print("warehouses:")
+    print(rows)
+
+    cs.execute("show databases")
+    rows = cs.fetchall()
+    print("databases:")
+    print(rows)
+
+    cs.execute("show roles")
+    rows = cs.fetchall()
+    print("roles:")
+    print(rows)
 finally:
     cs.close()
 ctx.close()
